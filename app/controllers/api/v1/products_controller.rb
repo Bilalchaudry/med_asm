bb2e6e2f981a8453c4b7b361d1ad8b6e210dca12
Bila <bilal.novatore@gmail.com>
2020-06-09 18:49:51 +0500

    searc medicine by name

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all 
+  end
+  def search_medicine
+    if params[:medicine_name].present?
+      @medicine = Product.find_by(name: params[:medicine_name]) rescue nil
+      if @medicine.nil?
+        bad_request_error('There is no medicine with that name .', 404)
+      end
+    else
+      bad_request_error('Required parameters are missing .', 404)
+    end
   end
 end
--- app/views/api/v1/products/index.json.jbuilder
+++ app/views/api/v1/products/index.json.jbuilder
@@ -1,12 +1,12 @@
 json.message "success"
 json.success true
 json.medicines @products.each do |product|
-    json.id product.id
-    json.name product.name
-    json.description product.description
-    json.cost product.cost
+  json.id product.id
+  json.name product.name
+  json.description product.description
+  json.cost product.cost
 
-  end
+end
 
 json.data @products.each do |product|
   json.id product.id
