c7a765c944ee0efd611eb443a9b3bb0ed54f20a9
Bila <bilal.novatore@gmail.com>
2020-06-12 10:17:46 +0500

    remove commented code

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -13,15 +13,5 @@ class Api::V1::ProductsController < ApiController
       @products = Product.all
     end
   end
-
-  # def search_medicine
-  #   if params[:medicine_name].present?
-  #     @medicine = Product.find_by(name: params[:medicine_name]) rescue nil
-  #     if @medicine.nil?
-  #       bad_request_error('There is no medicine with that name .')
-  #     end
-  #   else
-  #     bad_request_error('Required parameters are missing .')
-  #   end
-  # end
+  
 end
