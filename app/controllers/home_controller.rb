08a2e5982169dd4e07dd45eaad25e1d19b56d796
Bila <bilal.novatore@gmail.com>
2020-06-16 13:15:53 +0500

    error message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
+  include ErrorMessage
   before_action :authenticate_user!
   def page_content
     if params[:page_type].present?
       @page_content = Content.where(page_type: params[:page_type]).first
       if @page_content.nil?
-        render_error "Page content not found", 404
+        bad_request_error "Page content not found", 404
       end
     else
-      render_error "Missing required parameters", 400
+      bad_request_error "Missing required parameters", 400
     end
   end
 
