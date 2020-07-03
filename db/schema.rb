22cf4a4522e28e6518203141056e46b0754362a9
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-07-03 15:03:49 +0500

    uploa dpresciption api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_134036) do
+ActiveRecord::Schema.define(version: 2020_07_01_125803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_134036) do
     t.integer "quantity"
     t.integer "price"
     t.string "timing"
-    t.float "dose_quantity"
+    t.string "dose_quantity"
     t.string "comment"
     t.date "start_date"
     t.date "end_date"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_134036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status"
   end
 
   create_table "prescriptions", force: :cascade do |t|
